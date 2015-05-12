ea2b115816ce73fb999971e9e854ba65351f0c14
kavv212 <kvc2457@gmail.com>
2015-05-12 07:41:47 -0400

    Finished.

--- public/app.js
+++ public/app.js
@@ -8,7 +8,7 @@ app.controller('FlashCardController', function($scope) {
 			{text: 'Some kind of fish, right?', correct: false }
 		]
 	};
-	$scope.answerQuestion = function() {
-		console.log('I was called');
+	$scope.answerQuestion = function(theAnswer) {
+		console.log(theAnswer);
 	};
 });
