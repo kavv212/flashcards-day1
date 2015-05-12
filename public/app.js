319b250b3e4275452bf6c5c5eb391e35024a1bfb
kavv212 <kvc2457@gmail.com>
2015-05-12 02:51:36 -0400

    Finished the 'Making the Flash Card dynamic' section. -WIP

--- public/app.js
+++ public/app.js
@@ -1,2 +1,11 @@
 var app = angular.module('FlashCards', []);
-app.controller('FlashCardController', function() {});
+app.controller('FlashCardController', function($scope) {
+	$scope.flashCard = {
+		question: 'What is Angular?',
+		answers: [
+			{text: 'A front-end framework for great power!', correct: true },
+			{text: 'Something lame, who cares, whatever.', correct: false },
+			{text: 'Some kind of fish, right?', correct: false }
+		]
+	};
+});
